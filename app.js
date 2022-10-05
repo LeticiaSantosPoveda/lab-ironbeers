40776bf7e0de856701c954dcbf85705ec5fb851d
Leticia Santos <leticia.s.poveda@gmail.com>
2022-10-05 18:58:10 +0200

    solved lab

--- app.js
+++ app.js
@@ -13,13 +13,41 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
+hbs.registerPartials(path.join(__dirname, '/views/partials'));
 
-// ...
 
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
   res.render('index');
 });
+
+app.get('/beers', (req, res) => {    // con async await metemos el async delante de la función (req, res) y luego hacemos variable let beerArr = await...
+  punkAPI
+  .getBeers()
+  .then(beersFromApi => {
+    console.log('Beers from the database: ', beersFromApi)
+    const data = {
+      beers: beersFromApi         // es porque el render no me admite un array, lo meto dentro de un array.
+    }
+    res.render('beers', data);    // res.render dentro de la promise
+  })
+  .catch(error => console.log(error));
+
+});
+
+app.get('/random-beer', (req, res) => {
+  punkAPI
+  .getRandom()
+  .then(responseFromAPI => {
+    const randomData = {
+      randomBeers: responseFromAPI
+    }
+    res.render('random-beer', randomData);
+  })
+  .catch(error => console.log(error));
+});
+
+
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
